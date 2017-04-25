9cffd6bbb1c3b1eb5a40721be95979200553981a
Giraffen <Giraffen@DESKTOP-2GM47HH>
2017-04-25 19:23:08 +0200

    Ovning 1, server och client

---
+++ WebApiLabbar/Labb1 ClientOvning/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Labb1_ClientOvning.PersonServiceReference;
+
+namespace Labb1_ClientOvning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new PersonClient("BasicHttpBinding_IPerson");
+
+            Console.WriteLine(client.PrintAdress());
+            Console.WriteLine(client.PrintFullName());
+
+            Console.WriteLine();
+
+            client.EditFirstName("Sven");
+            client.EditLastName("Karlsson");
+            client.EditTel(042160497);
+            client.EditAdress("Smack");
+            Console.WriteLine(client.PrintAdress());
+            Console.WriteLine(client.PrintFullName());
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ WebApiLabbar/Labb1 ServerOvning/PersonService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb1_ServerOvning
+{
+    class PersonService : IPerson
+    {
+        private static Person person = new Person {
+            FirstName = "Jonny",
+            LastName = "Svensson",
+            Adress = "Brohultsbägen 222, 254 56 Helsingborg",
+            Tel = 0767878060
+        };
+        public bool EditAdress(string adress)
+        {
+            person.Adress = adress;
+            return true;
+        }
+
+        public bool EditFirstName(string name)
+        {
+            person.FirstName = name;
+            return true;
+        }
+
+        public bool EditLastName(string name)
+        {
+            person.LastName = name;
+            return true;
+        }
+
+        public bool EditTel(int tel)
+        {
+            person.Tel = tel;
+            return true;
+        }
+
+        public string PrintAdress()
+        {
+            return $"Tel: {person.Tel} Adress: {person.Adress}";
+        }
+
+        public string PrintFullName()
+        {
+            return $"Fullname: {person.FirstName} {person.LastName}";
+        }
+    }
+
+    [DataContract]
+    public class Person
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Adress { get; set; }
+        [DataMember]
+        public int Tel { get; set; }
+    }
+    [ServiceContract]
+    public interface IPerson
+    {
+        [OperationContract]
+        bool EditFirstName(string name);
+        [OperationContract]
+        bool EditLastName(string name);
+        [OperationContract]
+        bool EditAdress(string adress);
+        [OperationContract]
+        bool EditTel(int tel);
+        [OperationContract]
+        string PrintFullName();
+        [OperationContract]
+        string PrintAdress();
+    }
+}
