7c6e02003a98199789fa33df8f84c24acd2da7a6
Giraffen <Giraffen@DESKTOP-2GM47HH>
2017-04-25 14:39:11 +0200

    Lektion ett HelloWorldService

---
+++ WebApiLabbar/F01L01 - HelloWorldService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F01L01___HelloWorldService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ WebApiLabbar/F01L01/HelloWorldService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldService
+{
+    public class HelloWorldService : IHelloWorld
+    {
+        public string SayHello(Name person)
+        {
+            return $"Hello {person.FName} {person.LName}!";
+        }
+    }
+
+    [DataContract]
+    public class Name 
+    {
+        [DataMember]
+        public string FName { get; set; }
+        [DataMember]
+        public string LName { get; set; }
+    }
+    
+    [ServiceContract]
+    public interface IHelloWorld
+    {
+        [OperationContract]
+        string SayHello(Name person);
+    }
+}
