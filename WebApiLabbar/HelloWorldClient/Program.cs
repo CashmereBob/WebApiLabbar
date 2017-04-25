29efbf3c47fdea4e846bc9600cf0e12e7c7cf49e
Giraffen <Giraffen@DESKTOP-2GM47HH>
2017-04-25 15:41:39 +0200

    f01l01 client

---
+++ WebApiLabbar/HelloWorldClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using HelloWorldConsoleClient.HelloWorldServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new HelloWorldClient("BasicHttpBinding_IHelloWorld");
+
+            var name = new Name();
+
+            Console.Write("Firstname: ");
+            name.FName = Console.ReadLine();
+            Console.Write("Lastname: ");
+            name.LName = Console.ReadLine();
+
+            Console.WriteLine(client.SayHello(name));
+
+            Console.WriteLine();
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
